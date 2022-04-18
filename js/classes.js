c81223be106db5bd00d27abbf84790e939442dfe
DESKTOP-V3EO2BU\rlaxo <rlaxogudsla2@gmail.com>
2022-04-19 01:01:44 +0900

    22.04.19 / samuraiMack animate

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,5 @@
 class Sprite {
-    constructor({ position, imageSrc, scale = 1, framesMax = 1}){
+    constructor({ position, imageSrc, scale = 1, framesMax = 1, offset = {x: 0, y:0} }){
         this.position = position
         this.width = 50
         this.height = 150
@@ -10,6 +10,7 @@ class Sprite {
         this.frameCurrent = 0
         this.framesElapsed = 0
         this.framesHold = 5
+        this.offset = offset
     }
 
     draw(){
@@ -19,8 +20,8 @@ class Sprite {
             0,
             this.image.width / this.framesMax,
             this.image.height,
-            this.position.x, 
-            this.position.y, 
+            this.position.x - this.offset.x, 
+            this.position.y - this.offset.y, 
             (this.image.width / this.framesMax)* this.scale, 
             this.image.height * this.scale
             )
@@ -43,9 +44,24 @@ class Sprite {
 
 
 
-class Fighter {
-    constructor({position, velocity, color = 'red', offset}){
-        this.position = position
+class Fighter extends Sprite{
+    constructor({
+        position, 
+        velocity, 
+        color = 'red', 
+        imageSrc, 
+        scale = 1, 
+        framesMax = 1,
+        offset = {x: 0, y:0}
+    }){
+        super({
+            position,
+            imageSrc,
+            scale,
+            framesMax,
+            offset
+        })
+
         this.velocity = velocity
         this.width = 50
         this.height = 150
@@ -62,24 +78,12 @@ class Fighter {
         this.color = color
         this.isAttacking
         this.health = 100
+        this.frameCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 5
     }
 
-    draw(){
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
-        // attackBox
-       if(this.isAttacking){
-            c.fillStyle = 'green'
-            c.fillRect(
-                this.attackBox.position.x,
-                this.attackBox.position.y,
-                this.attackBox.width,
-                this.attackBox.height
-            )
-        }
-
-    }
 
     update(){
         this.draw()
