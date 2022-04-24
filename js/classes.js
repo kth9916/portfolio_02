3963716c79a43df64ab901b55ce7d9627730b4ce
DESKTOP-V3EO2BU\rlaxo <rlaxogudsla2@gmail.com>
2022-04-25 04:50:03 +0900

    22.04.25 - Player Sprite (Samurai Mack) - Run

--- js/classes.js
+++ js/classes.js
@@ -56,7 +56,8 @@ class Fighter extends Sprite{
         imageSrc, 
         scale = 1, 
         framesMax = 1,
-        offset = {x: 0, y:0}
+        offset = {x: 0, y:0},
+        sprites
     }){
         super({
             position,
@@ -85,6 +86,14 @@ class Fighter extends Sprite{
         this.frameCurrent = 0
         this.framesElapsed = 0
         this.framesHold = 5
+        this.sprites = sprites
+
+        for(const sprite in this.sprites){
+            sprites[sprite].image = new Image()
+            sprites[sprite].image.src = sprites[sprite].imageSrc
+        }
+
+        
     }
 
 
@@ -92,7 +101,7 @@ class Fighter extends Sprite{
     update(){
         this.draw()
         this.animateFrame()
-        
+
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
         
