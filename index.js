962da1520857e9f002e04f5d3a923b19f15d6045
DESKTOP-V3EO2BU\rlaxo <rlaxogudsla2@gmail.com>
2022-05-02 08:40:24 +0900

    22.05.02 - React to Sprite Attacks

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ const player = new Fighter({
             imageSrc: './img/samuraiMack/Attack1.png',
             framesMax: 6
         }
+    },
+    attackBox: {
+        offset: {
+            x: 100,
+            y: 50
+        },
+        width: 160,
+        height: 50
     }
 })
 
@@ -114,6 +122,14 @@ const enemy = new Fighter({
             imageSrc: './img/kenji/Attack1.png',
             framesMax: 4
         }
+    },
+    attackBox: {
+        offset: {
+            x: -170,
+            y: 50
+        },
+        width: 170,
+        height: 50
     }
 })
 
@@ -195,11 +211,16 @@ function animate(){
             rectangle1: player,
             rectangle2: enemy
         }) &&
-        player.isAttacking
+        player.isAttacking && player.frameCurrent === 4
     ) {
         player.isAttacking = false
         enemy.health -= 20
         document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+    }
+
+    // if player misses
+    if(player.isAttacking && player.frameCurrent === 4){
+        player.isAttacking = false
     }
 
     if(
@@ -207,11 +228,16 @@ function animate(){
             rectangle1: enemy,
             rectangle2: player
         }) &&
-        enemy.isAttacking
+        enemy.isAttacking && enemy.frameCurrent === 2
     ) {
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // if enemy misses
+    if(enemy.isAttacking && enemy.frameCurrent === 2){
+        enemy.isAttacking = false
     }
 
     // end game based ond health
