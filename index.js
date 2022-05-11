178f4fcc23e91abaa50a9a234f5d2420951b0684
kth9916 <rlaxogudsla2@gmail.com>
2022-05-12 03:18:50 +0900

    22.05.12 - add bgm

--- index.js
+++ index.js
@@ -333,3 +333,18 @@ window.addEventListener('keyup', (event) => {
         break
     }
 })
+
+// btn1을 눌렀을 때 sound1.mp3 재생
+document.querySelector(".btn1").addEventListener("click", function () {
+    var audio1 = new Audio("music/sound1.mp3");
+    audio1.loop = false; // 반복재생하지 않음
+    audio1.volume = 0.3; // 음량 설정
+    audio1.play(); // sound1.mp3 재생
+  });
+   
+  // btn2를 눌렀을 때 sound2.mp3 재생
+  document.querySelector(".btn2").addEventListener("click", function () {
+    var audio1 = new Audio("music/sound1.mp3");
+    audio1.pause();
+  });
+
